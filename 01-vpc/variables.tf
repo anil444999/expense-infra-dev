596d05d748ee5173ebf8143e55670d21f509fb9b
aniljoindevops <yerramaniianilkumar@gmail.com>
2024-06-04 21:03:46 +0530

    sg

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -3,7 +3,7 @@ variable "project_name" {
 }
 
 variable "environment" {
-  default = "dev" 
+  default = "dev"
 }
 
 variable "common_tags" {
@@ -11,21 +11,21 @@ variable "common_tags" {
         Project = "expense"
         Environment = "dev"
         Terraform = "true"
-    }  
+    }
 }
 
-variable "public_subnet_cidr" {
-    default = ["10.0.1.0/24","10.0.2.0/24"]  
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
-variable "private_subnet_cidr" {
-    default = ["10.0.11.0/24","10.0.12.0/24"]  
+variable "private_subnet_cidrs" {
+    default = ["10.0.11.0/24","10.0.12.0/24"]
 }
 
-variable "database_subnet_cidr" {
-    default = ["10.0.21.0/24","10.0.22.0/24"]  
+variable "database_subnet_cidrs" {
+    default = ["10.0.21.0/24","10.0.22.0/24"]
 }
 
 variable "is_peering_required" {
-  default = true  
+  default = true
 }
